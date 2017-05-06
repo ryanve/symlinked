937ed2aef1d2c56aa86681ae7a0ed4c99fc721f5
Ryan Van Etten <ryanve@gmail.com>
2017-05-06 16:14:09 -0700

    use is-array-of

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
 var symlinked = require("./")
+var isArrayOf = require("is-array-of")
+var isStrings = isArrayOf("string")
+var isObjects = isArrayOf("object")
 
 function ok(message, test) {
   if (test) console.info("Pass: " + message)
   else throw new Error("Fail: " + message)
 }
 
-ok(".search return type", Array.isArray(symlinked.search()))
-ok(".names return type", Array.isArray(symlinked.names()))
-ok(".paths return type", Array.isArray(symlinked.paths()))
-ok(".links return type", Array.isArray(symlinked.links()))
+ok(".search return type", isObjects(symlinked.search()))
+ok(".names return type", isStrings(symlinked.names()))
+ok(".paths return type", isStrings(symlinked.paths()))
+ok(".links return type", isStrings(symlinked.links()))
+ok(".deps return type", isStrings(symlinked.links()))
 ok(".is return type", typeof symlinked.is(".") == "boolean")
 
 console.log("All tests passed =)")
